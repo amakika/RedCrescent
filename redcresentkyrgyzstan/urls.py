8e15c87eabea7d679c3c3346d8bf3de13b136f02
Yasin384 <72139082+Yasin384@users.noreply.github.com>
2025-01-11 22:07:50 +0600

    Update urls.py

--- redcresentkyrgyzstan/urls.py
+++ redcresentkyrgyzstan/urls.py
@@ -18,10 +18,17 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('volonteer.urls'))
+    path('api/',include('volonteer.urls')),
+     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # получение токена
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # обновление токена
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+
+
 
