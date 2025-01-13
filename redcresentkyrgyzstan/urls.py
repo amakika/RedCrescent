0a6442317c40fcf0c1e78468d576208266e1e6c8
Yasin384 <72139082+Yasin384@users.noreply.github.com>
2025-01-13 21:18:59 +0600

    Update urls.py

--- redcresentkyrgyzstan/urls.py
+++ redcresentkyrgyzstan/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for redcresentkyrgyzstan project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
@@ -24,31 +8,29 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from rest_framework.authentication import TokenAuthentication
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from rest_framework import permissions
-from django.views.generic import RedirectView
+
+# Import AllowAny
+from rest_framework.permissions import AllowAny
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Red Crescent API",
         default_version="v1",
         description="API documentation for the Red Crescent project",
+        terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="asinarstanbekov51@gmail.com"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=(AllowAny,),
+    permission_classes=(AllowAny,),  # Use AllowAny for public access to Swagger
 )
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-   
-    path('api/',include('volonteer.urls')),
-     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # получение токена
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # обновление токена
-    
+    path('accounts/login/', include('rest_framework.urls')),  # For login/logout views
+    path('api/', include('volonteer.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # Swagger and Redoc URLs
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
@@ -58,8 +40,6 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
 
 
 
