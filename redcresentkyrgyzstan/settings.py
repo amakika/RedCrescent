f953248f71b14fcfd14d09df175e0994fb3f99bf
Yasin384 <72139082+Yasin384@users.noreply.github.com>
2025-01-13 20:56:03 +0600

    Update settings.py

--- redcresentkyrgyzstan/settings.py
+++ redcresentkyrgyzstan/settings.py
@@ -138,14 +138,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
+
 
 
 # Internationalization
