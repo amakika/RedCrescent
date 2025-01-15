0d5dbe4f18e2773b59fda3d0ca71af16c5f9bcb2
Yasin384 <asinarstanbekov51@gmail.com>
2025-01-15 21:18:24 +0600

    offing cors and crsf

--- redcresentkyrgyzstan/settings.py
+++ redcresentkyrgyzstan/settings.py
@@ -22,8 +22,6 @@ ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
-CSRF_TRUSTED_ORIGINS = ['*']  # Trust all origins for CSRF in development
-
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -55,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-   # 'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
